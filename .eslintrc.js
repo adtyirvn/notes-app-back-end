cfb0290503c070cdcd4e8c8d8eb7be7afe36cd0f
Aditya Irvan <adityairvanku@gmail.com>
2023-03-28 00:40:20 +0700

    menyimpan catatan

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,14 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: ['airbnb-base', 'prettier'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
-  plugins: ["prettier"],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": ["error"],
+    'no-console': 'off',
+    'prettier/prettier': ['error', { singleQuote: true }],
   },
 };
