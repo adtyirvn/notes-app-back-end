47a6259cff80bf59f463b96e13646006bcc478e0
adty44 <adityairvanku@gmail.com>
2023-03-28 06:35:37 +0700

    menghapus catatan

--- src/routes.js
+++ src/routes.js
@@ -26,6 +26,11 @@ const routes = [
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
   },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
 ];
 
 module.exports = routes;
