f41c6ab6fe589bf3f1d1a02e36945973086e515e
Aditya Irvan <adityairvanku@gmail.com>
2023-04-25 22:04:59 +0700

    add storage service

---
+++ src/api/uploads/routes.js
@@ -0,0 +1,17 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/upload/images',
+    handler: handler.postUploadImageHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true,
+        output: 'stream',
+      },
+      auth: 'notesapp_jwt',
+    },
+  },
+];
+
+module.exports = routes;
